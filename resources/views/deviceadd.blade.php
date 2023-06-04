4064f2b4952f910e6783211373ef5481f33f0868
rihenn <113469256+rihenn@users.noreply.github.com>
2023-06-04 19:20:13 +0300

    tablo adları düzeltildi users yazdır kısmı insertorignore olarak değiştirildi modeller düzenlendi tablo adlarına göre

--- resources/views/deviceadd.blade.php
+++ resources/views/deviceadd.blade.php
@@ -214,7 +214,7 @@
                     <form action="{{ route('diveceAdd') }}" method="post">
                             @csrf
                             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <h6 class="mb-2 text-primary">Cihaz Ekle</h6>
+                                <h6 class="mb-2 text-primary">kapı Ekle</h6>
                             </div>
                             <div>
                                
